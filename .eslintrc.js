ec1c0a8548549dfcbb00774e3c54f2dfd3de8af6
Gatien Anizan <gatien.anizan@gmail.com>
2020-04-15 21:19:58 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,8 @@ module.exports = {
     '@vue/typescript',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
     'max-len': 'off',
     'consistent-return': 'off',
     'import/prefer-default-export': 'off',
